10be7cb20bf8ce982a79d2ce6a007da38ed4ae78
eliotkaplan <kaplan.eliotj@gmail.com>
2022-07-10 17:54:14 -0400

    arbitrary polygon emitters

--- axi_art/sun_rays/sun_rays.py
+++ axi_art/sun_rays/sun_rays.py
@@ -6,87 +6,98 @@ import axi
 import click
 import numpy as np
 from axi import Drawing
-from shapely.geometry import LineString, MultiLineString, Polygon, GeometryCollection
+from shapely.affinity import rotate
+from shapely.geometry import (
+    LineString,
+    MultiLineString,
+    Polygon,
+    GeometryCollection,
+    Point,
+)
 
 rng = np.random.default_rng()
 
 
-class GrowthLine:
-    def __init__(self, anchor: tuple[float, float], speed: float, angle: float):
-        self.anchor = np.array(anchor)
-        self.endpoint = np.array(anchor)
-        self.velocity = speed * np.array([np.cos(angle), np.sin(angle)])
-
-    @property
-    def linestring(self) -> LineString:
-        return LineString([self.anchor, self.endpoint])
-
-    @property
-    def next_linestring(self) -> LineString:
-        return LineString([self.anchor, self.endpoint + self.velocity])
-
-    def grow(self) -> None:
-        self.endpoint += self.velocity
-
-
-class RadialSun:
-    def __init__(self, center: tuple[float, float], spokes: int, center_radius: float):
-        center = np.array(center)
-        offset = rng.uniform(0, 2 * np.pi / spokes)
-        self.spokes = [
-            GrowthLine(
-                center
-                + center_radius
-                * np.array([np.cos(angle + offset), np.sin(angle + offset)]),
-                0.01,
-                angle,
-            )
-            for angle in np.linspace(0, 2 * np.pi, spokes, endpoint=False)
-        ]
-
-    @property
-    def linestrings(self) -> list[LineString]:
-        return [spoke.linestring for spoke in self.spokes]
-
-    @property
-    def central_disc(self) -> Polygon:
-        return Polygon([spoke.anchor for spoke in self.spokes])
-
-    def grow(self, obstacle: MultiLineString, sep: float) -> bool:
-        growth = False
-        for spoke in self.spokes:
-            if spoke.next_linestring.distance(obstacle) >= sep:
-                spoke.grow()
-                growth = True
-        return growth
-
-    @property
-    def drawing(self) -> Drawing:
-        return Drawing([(spoke.anchor, spoke.endpoint) for spoke in self.spokes]).sort_paths()
+def nearest_point(p: Point, c: GeometryCollection) -> Point:
+    if isinstance(c, Point):
+        return c
+    points = []
+    if isinstance(c, LineString):
+        points += [Point(coord) for coord in c.coords]
+    else:
+        for g in c.geoms:
+            if isinstance(g, Point):
+                points.append(g)
+            else:
+                for x in g.coords:
+                    points.append(Point(x))
+    best = None
+    best_dist = float("inf")
+    for elem in points:
+        if (dist := elem.distance(p)) < best_dist:
+            best_dist = dist
+            best = elem
+    return best
 
 
-def radial_sun_drawing(
-    width: float, height: float, suns: int, spokes: int, radius: float
+def radial_drawing(
+    width: float,
+    height: float,
+    shapes: list[Polygon],
+    n_lines: int,
 ) -> list[Drawing]:
     boundary = LineString([(0, 0), (width, 0), (width, height), (0, height), (0, 0)])
-    r = min(width, height) / 4
-    centers = [
-        (width / 2 + r * np.cos(angle), height / 2 + r * np.sin(angle))
-        for angle in np.linspace(0, 2 * np.pi, suns, endpoint=False)
-    ]
-    suns = [RadialSun(center, spokes, radius) for center in centers]
-    obstacles = {
-        sun: GeometryCollection([boundary, *[x.central_disc for x in suns if x != sun]])
-        for sun in suns
-    }
-    while True:
-        growth = False
-        for sun in suns:
-            growth = growth or sun.grow(obstacles[sun], 0.01)
-        if not growth:
-            break
-    drawings = [sun.drawing for sun in suns]
+    drawings = []
+    for i, shape in enumerate(shapes):
+        obstacles = GeometryCollection([boundary, *[x for x in shapes if x != shape]])
+        lines = []
+        center = np.array(shape.centroid.coords[0])
+        angle_offset = i * 2 * np.pi / n_lines / len(shapes)
+        for angle in np.linspace(
+            angle_offset, 2 * np.pi + angle_offset, n_lines, endpoint=False
+        ):
+            velocity = 1000 * np.array([np.cos(angle), np.sin(angle)])
+            ray = LineString([center, center + velocity])
+            start = shape.exterior.intersection(ray)
+            end = nearest_point(start, obstacles.intersection(ray))
+            if None in (start, end) or start.distance(end) < 1e-5:
+                continue
+            lines.append([start.coords[0], end.coords[0]])
+        drawings.append(Drawing(lines))
     return drawings
+
+
+def n_gon(n: int, x: float, y: float, r: float) -> Polygon:
+    return Polygon(
+        [
+            (x + r * np.cos(angle), y + r * np.sin(angle))
+            for angle in np.linspace(0, 2 * np.pi, n, endpoint=False)
+        ]
+    )
+
+
+def random_n_gons(
+    width: float, height: float, min_r: float, max_r: float, shapes: int, separation: float
+) -> list[Polygon]:
+    out: list[Polygon] = []
+    sides = rng.choice([3, 4, 6, 200], shapes, False)
+    while len(out) < shapes:
+        r = rng.uniform(min_r, max_r)
+        shape = n_gon(
+            sides[len(out)],
+            rng.uniform(r, width - r),
+            rng.uniform(r, height - r),
+            r,
+        )
+        shape = rotate(shape, rng.uniform(0, 360), "centroid")
+        good = True
+        for other in out:
+            if other.distance(shape) < separation:
+                good = False
+                break
+        if good:
+            out.append(shape)
+    return out
 
 
 @click.command()
@@ -95,20 +106,24 @@ def radial_sun_drawing(
 @click.option("-h", "--height", prompt=True, type=float, default=8)
 @click.option("-m", "--margin", prompt=True, type=float, default=0.5)
 @click.option("-s", "--spokes", prompt=True, type=int, default=100)
-@click.option("-n", "--suns", prompt=True, type=int)
-@click.option("-r", "--radius", prompt=True, type=float, default=1.0)
+@click.option("-n", "--emitters", prompt=True, type=int)
+@click.option("-minr", "--min_radius", prompt=True, type=float, default=0.5)
+@click.option("-maxr", "--max_radius", prompt=True, type=float, default=1.5)
+@click.option("-sep", "--separation", prompt=True, type=float, default=0.5)
 def main(
     test: bool,
     width: float,
     height: float,
     margin: float,
     spokes: int,
-    suns: int,
-    radius: float,
+    emitters: int,
+    min_radius: float,
+    max_radius: float,
+    separation: float,
 ):
-    drawings = radial_sun_drawing(
-        width - 2 * margin, height - 2 * margin, suns, spokes, radius
-    )
+    dw, dh = width - 2 * margin, height - 2 * margin
+    emitters = random_n_gons(dw, dh, min_radius, max_radius, emitters, separation)
+    drawings = radial_drawing(dw, dh, emitters, spokes)
     drawings = Drawing.multi_scale_to_fit(drawings, width, height, margin)
 
     if test or axi.device.find_port() is None:
